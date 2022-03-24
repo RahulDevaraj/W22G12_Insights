97597e512a18fdb5ca9f70a2b0b99b838abce67a
RahulDevaraj <kannan.vegeta@gmail.com>
2022-03-23 17:45:12 -0700

    Forgot Password + Email Reset Completed

--- app/src/main/java/com/example/insights/ForgotPassword.java
+++ app/src/main/java/com/example/insights/ForgotPassword.java
@@ -1,13 +1,27 @@
 package com.example.insights;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.insights.databases.UserDatabase;
+import com.example.insights.helpers.MailHelper;
+import com.example.insights.interfaces.UserDao;
+import com.example.insights.model.User;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.mail.MessagingException;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -21,21 +35,73 @@ public class ForgotPassword extends AppCompatActivity {
         btnSubmit = findViewById(R.id.btnSubmit);
         edtTxtEmail = findViewById(R.id.edtTxtUserId);
 
+
         btnSubmit.setOnClickListener((View view)-> {
             if(!android.util.Patterns.EMAIL_ADDRESS.matcher(edtTxtEmail.getText().toString()).matches())
             Toast.makeText(this, "Invalid Email", Toast.LENGTH_SHORT).show();
             else{
-                // check DB for valid Registered Email
-                // update to random password
-                // send email with new password
-                //return to login
 
-                Toast.makeText(this, "Email Sent Successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(ForgotPassword.this,MainActivity.class));
+
+                UserDatabase database;
+                database = Room.databaseBuilder(getApplicationContext(), UserDatabase.class,"User.db").build();
+
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                executorService.execute(()-> {
+                    UserDao userDao = database.userDao();
+                    // check DB for valid Registered Email
+                    User user = userDao.findUserByEmail(edtTxtEmail.getText().toString());
+
+                    runOnUiThread(()-> {
+                        if(user==null){
+                            Toast.makeText(this, "This user does not exist", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            // check Generate password and Reset
+                            String newPassword = generatePassword();
+                            String subject = "Password Reset Mail";
+                            String message = "Please use the following password to login to the account : "+newPassword;
+                            try{
+                                sendMail(user.getEmailId(),subject,message);
+                                executorService.execute(()->{
+                                    userDao.UpdatePassword(newPassword,user.getEmailId());
+                                });
+                                runOnUiThread(()->{
+                                    Toast.makeText(this, "Email Sent Successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(ForgotPassword.this,MainActivity.class));
+                                });
+                            }
+                            catch (MessagingException e){
+                                e.printStackTrace();
+                                Toast.makeText(this, "Failed to send Email", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                    });
+                });
+
             }
+
+
 
         });
 
     }
+    private void sendMail(String senderAddress,String subject,String message) throws MessagingException {
+        MailHelper mailHelper = new MailHelper(ForgotPassword.this, senderAddress, subject, message);
+
+        mailHelper.execute();
+    }
+
+    private String generatePassword(){
+        String password="";
+        Random random = new Random();
+        for(int i=0;i<6;i++){
+            int digit = random.nextInt(10);
+            password += digit;
+        }
+        return password;
+    }
+
 
 }
