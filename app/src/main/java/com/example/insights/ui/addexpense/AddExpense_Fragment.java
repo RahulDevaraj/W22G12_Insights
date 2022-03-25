56b0c7edc953c6f9b4d4d9b4477565358db6fb13
basjosben <basjosben@gmail.com>
2022-03-25 14:20:57 -0700

    AddExpenseFragment

--- app/src/main/java/com/example/insights/ui/addexpense/AddExpense_Fragment.java
+++ app/src/main/java/com/example/insights/ui/addexpense/AddExpense_Fragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.insights.databinding.AddExpenseFragmentBinding;
+
+import org.w3c.dom.Text;
 
 public class AddExpense_Fragment extends Fragment {
 
@@ -21,8 +27,20 @@ public class AddExpense_Fragment extends Fragment {
         binding = AddExpenseFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        TextView textView = binding.textAddExpense;
-        textView.setText("Add Expense Fragment");
+        TextView textViewDescription = binding.txtDesc;
+        EditText editTxtDescription = binding.editTxtDescription;
+        TextView textViewCategory = binding.txtCategory;
+        Spinner spinnerCategoryTypes = binding.spinnerCategoryTypes;
+        TextView textViewDate = binding.txtDate;
+        CalendarView calendarViewDate = binding.calendarViewDate;
+        Button btnAddExpense = binding.btnAddExpense;
+
+        //float scalingFactor = 0.8f; // scale down to half the size view.
+        //calendarViewDate.setScaleX(scalingFactor);
+        //calendarViewDate.setScaleY(scalingFactor);
+
+
+        //textViewDescription.setText("Add Expense Fragment");
 
         return root;
     }
