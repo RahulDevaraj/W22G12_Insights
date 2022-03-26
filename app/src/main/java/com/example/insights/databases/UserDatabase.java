2a9fc5583e43731971d3b6819f77a4acb1004543
RahulDevaraj <kannan.vegeta@gmail.com>
2022-03-26 13:22:50 -0700

    DB version changed to 1

--- app/src/main/java/com/example/insights/databases/UserDatabase.java
+++ app/src/main/java/com/example/insights/databases/UserDatabase.java
@@ -8,7 +8,7 @@ import com.example.insights.interfaces.UserTransactionDao;
 import com.example.insights.model.User;
 import com.example.insights.model.UserTransaction;
 
-@Database(entities = {User.class, UserTransaction.class},version=2,exportSchema = false)
+@Database(entities = {User.class, UserTransaction.class},version=1,exportSchema = false)
 public abstract class UserDatabase extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract UserTransactionDao userTransactionDao();
