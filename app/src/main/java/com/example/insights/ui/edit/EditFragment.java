7d7cd9d2d73f958429bc6daa89457fca0216b30d
Asha-Anthony <asha.anthony95@gmail.com>
2022-04-02 12:06:29 -0700

    Edit expense data preview

--- app/src/main/java/com/example/insights/ui/edit/EditFragment.java
+++ app/src/main/java/com/example/insights/ui/edit/EditFragment.java
@@ -8,12 +8,16 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.insights.R;
 import com.example.insights.databinding.EditFragmentBinding;
 import com.example.insights.model.UserTransaction;
 import com.google.gson.Gson;
@@ -21,7 +25,7 @@ import com.google.gson.Gson;
 
 public class EditFragment extends Fragment {
 
-    EditText test ;
+  ;
 
     private EditFragmentBinding binding;
 
@@ -34,8 +38,37 @@ public class EditFragment extends Fragment {
         Gson gson = new Gson();
         String json = sharedPreferences.getString("USERTRANSACTION", "");
         UserTransaction userTransaction = gson.fromJson(json, UserTransaction.class);
-        test = binding.editTxtDescription;
-        test.setText(""+userTransaction.getDescription());
+
+        EditText editTxtDescription = binding.editTxtDescription;
+        EditText editTxtAmount = binding.editTxtAmount;
+        Spinner spinnerCategoryTypes = binding.spinnerCategoryTypes;
+        DatePicker datePicker = binding.datePicker;
+        Button btnSave = binding.btnSave;
+        Button btnCancel = binding.btnSave;
+
+        editTxtDescription.setText(""+userTransaction.getDescription());
+        editTxtAmount.setText(""+userTransaction.getAmount());
+        String Category = userTransaction.getCategory();
+        int spinnerPosition = -1;
+        switch (Category){
+            case "Shopping":
+                spinnerPosition = 0;
+                break;
+            case"Pets":
+                spinnerPosition = 1;
+                break;
+            case"Travel":
+                spinnerPosition = 2;
+                break;
+            case"Personal":
+                spinnerPosition = 3;
+                break;
+        }
+        spinnerCategoryTypes.setSelection(spinnerPosition);
+
+        String date[] = userTransaction.getTxndate().split("-");
+
+        datePicker.updateDate(Integer.parseInt(date[2]),Integer.parseInt(date[0])-1, Integer.parseInt(date[1]));
 
         return root;
     }
