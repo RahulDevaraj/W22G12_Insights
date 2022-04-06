f010d5a6e34c5ccb988d8d40b69accfa2afec04a
Asha-Anthony <asha.anthony95@gmail.com>
2022-04-05 20:49:03 -0700

    google sign in

--- app/src/main/java/com/example/insights/activities/MainActivity.java
+++ app/src/main/java/com/example/insights/activities/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,22 +23,32 @@ import com.example.insights.R;
 import com.example.insights.databases.UserDatabase;
 import com.example.insights.interfaces.UserDao;
 import com.example.insights.model.User;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox show_hide_password;
-    EditText edtTextPassword , edtTextUserId;
-    TextView txtViewFrgtPass,txtViewCreateAccount;
+    EditText edtTextPassword, edtTextUserId;
+    TextView txtViewFrgtPass, txtViewCreateAccount;
+    ImageView imgViewGoogle;
     Button btnLogin;
     UserDatabase db;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar a  = getSupportActionBar();
+        ActionBar a = getSupportActionBar();
         a.setDisplayShowHomeEnabled(true);
         a.setDisplayUseLogoEnabled(true);
         a.setLogo(R.mipmap.ic_launcher_logo);
@@ -48,8 +59,19 @@ public class MainActivity extends AppCompatActivity {
         txtViewFrgtPass = findViewById(R.id.forgot_password);
         txtViewCreateAccount = findViewById(R.id.txtViewCreateAccount);
         btnLogin = findViewById(R.id.BtnLogin);
+        imgViewGoogle = findViewById(R.id.imgViewGoogle);
 
-        show_hide_password.setOnCheckedChangeListener((CompoundButton compoundButton, boolean isChecked)-> {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+        imgViewGoogle.setOnClickListener((View view) -> {
+            SignIn();
+
+        });
+
+        show_hide_password.setOnCheckedChangeListener((CompoundButton compoundButton, boolean isChecked) -> {
 
             if (!isChecked) {
                 // hide password
@@ -61,48 +83,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        txtViewFrgtPass.setOnClickListener((View view)-> {
-            Intent myIntent = new Intent(MainActivity.this,ForgotPassword.class);
+        txtViewFrgtPass.setOnClickListener((View view) -> {
+            Intent myIntent = new Intent(MainActivity.this, ForgotPassword.class);
             startActivity(myIntent);
         });
 
-        txtViewCreateAccount.setOnClickListener((View view)-> {
-            startActivity(new Intent(MainActivity.this,Reggister.class));
+        txtViewCreateAccount.setOnClickListener((View view) -> {
+            startActivity(new Intent(MainActivity.this, Reggister.class));
         });
 
-        db = Room.databaseBuilder(getApplicationContext(),UserDatabase.class,"User.db").build();
+        db = Room.databaseBuilder(getApplicationContext(), UserDatabase.class, "User.db").build();
         UserDao userDao = db.userDao();
-        btnLogin.setOnClickListener((View view) ->{
-            if(edtTextUserId.getText().toString().isEmpty() || edtTextPassword.getText().toString().isEmpty())
-            {
+        btnLogin.setOnClickListener((View view) -> {
+            if (edtTextUserId.getText().toString().isEmpty() || edtTextPassword.getText().toString().isEmpty()) {
                 Toast.makeText(this, "User Id and Passwords Fields cannot be empty", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
+            } else {
                 ExecutorService executorService = Executors.newSingleThreadExecutor();
-                executorService.execute(() ->{
+                executorService.execute(() -> {
 
                     User user = userDao.findUserById(edtTextUserId.getText().toString());
-                    runOnUiThread(()-> {
-                        if (user == null){
-                            Toast.makeText(MainActivity.this,"Account does not exist.Register to Login",Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            if (user.getPassword().equals(edtTextPassword.getText().toString()) ) {
+                    runOnUiThread(() -> {
+                        if (user == null) {
+                            Toast.makeText(MainActivity.this, "Account does not exist.Register to Login", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (user.getPassword().equals(edtTextPassword.getText().toString())) {
                                 runOnUiThread(() -> {
                                     //if successfull put the details inside a shared preference
                                     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
                                     SharedPreferences.Editor editor = sharedPreferences.edit();
-                                    editor.putString("USERID",user.getUserId());
-                                    editor.putString("USEREMAIL",user.getEmailId());
-                                    editor.putString("USERLIMIT",user.getAmtLimit().toString());
+                                    editor.putString("USERID", user.getUserId());
+                                    editor.putString("USEREMAIL", user.getEmailId());
+                                    editor.putString("USERLIMIT", user.getAmtLimit().toString());
                                     editor.commit();
 
                                     //home activity called
                                     startActivity(new Intent(MainActivity.this, HomePage.class));
                                 });
                             } else {
-                                runOnUiThread(()-> {
+                                runOnUiThread(() -> {
                                     Toast.makeText(MainActivity.this, "Incorrect Password. Try Again", Toast.LENGTH_SHORT).show();
                                 });
 
@@ -112,5 +130,29 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    private void SignIn() {
+
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                startActivity(new Intent(MainActivity.this, HomePage.class));
+
+            } catch (ApiException e) {
+                e.getMessage();
+
+            }
+
+        }
     }
 }
